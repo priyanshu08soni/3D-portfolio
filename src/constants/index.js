18147f66a7fb70e9437226f94d0917ee6f9a86bd
PRIYANSHU SONI <122470603+priyanshu08soni@users.noreply.github.com>
2024-10-22 19:24:37 +0530

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,228 @@
+export const navLinks = [
+  {
+    id: 1,
+    name: 'Home',
+    href: '#home',
+  },
+  {
+    id: 2,
+    name: 'About',
+    href: '#about',
+  },
+  {
+    id: 3,
+    name: 'Work',
+    href: '#work',
+  },
+  {
+    id: 4,
+    name: 'Contact',
+    href: '#contact',
+  },
+];
+
+
+
+export const myProjects = [
+  {
+    title: 'Market Screeners - A Stock Market Website',
+    desc: 'This is a stock market website that utilizes historical data ( Kaggle - Nifty - 50 ) to give insights related to the stock trends. It includes data visual-charts, and calculated terms.',
+    subdesc:
+      'Built as a unique Software-as-a-Service app with React.js, Tailwind CSS, JavaScript, Market Screeners is designed for optimal performance and scalability.',
+    href: 'https://ms-stock-market-website.vercel.app/',
+    texture: '/textures/project/project1.mp4',
+    logo: '/assets/MS.svg',
+    logoStyle: {
+      backgroundColor: '#2A1816',
+      border: '0.2px solid #36201D',
+      boxShadow: '0px 0px 60px 0px #AA3C304D',
+    },
+    spotlight: '/assets/spotlight1.png',
+    tags: [
+      {
+        id: 1,
+        name: 'React.js',
+        path: '/assets/react.svg',
+      },
+      {
+        id: 2,
+        name: 'TailwindCSS',
+        path: 'assets/tailwindcss.png',
+      },
+      {
+        id: 3,
+        name: 'JavaScript',
+        path: '/assets/JS.png',
+      }
+    ],
+  },
+  {
+    title: 'ALABAY - A Gaming Website',
+    desc: 'Where the legendary central asian shepherd dog meets a new-age adventure. Join Us in celebrating the strength, loyality and heritage of the alabay breed.',
+    subdesc:
+      'With ALABAY, users can experience the immersive adventure, where multiple players work together in real time without any lag, by using React.js newest features.',
+    href: 'https://alabay-game.vercel.app/',
+    texture: '/textures/project/project2.mp4',
+    logo: '/assets/history.png',
+    logoStyle: {
+      backgroundColor: '#13202F',
+      border: '0.2px solid #17293E',
+      boxShadow: '0px 0px 60px 0px #2F6DB54D',
+    },
+    spotlight: '/assets/spotlight2.png',
+    tags: [
+      {
+        id: 1,
+        name: 'React.js',
+        path: '/assets/react.svg',
+      },
+      {
+        id: 2,
+        name: 'TailwindCSS',
+        path: 'assets/tailwindcss.png',
+      },
+      {
+        id: 3,
+        name: 'JavaScript',
+        path: '/assets/JS.png',
+      }
+    ],
+  },
+  {
+    title: 'BlogsTop - A news viewing app with profile management.',
+    desc: 'An innovative news app with infinite scroll and different categories. With Profile management ( Create and Update ).',
+    subdesc:
+      'With a focus on efficiency, BlogsTop uses React, Express.js, Node.js and MongoDB to create a unique design for the web-app.',
+    href: 'https://drive.google.com/file/d/1z5QKJ307fyON-SN3cJ8jKZFDkFUQ-kYA/view?usp=sharing',
+    texture: '/textures/project/project3.mp4',
+    logo: '/assets/blog-logo.jpg',
+    logoStyle: {
+      backgroundColor: '#60f5a1',
+      background:
+        'linear-gradient(0deg, #60F5A150, #60F5A150), linear-gradient(180deg, rgba(255, 255, 255, 0.9) 0%, rgba(208, 213, 221, 0.8) 100%)',
+      border: '0.2px solid rgba(208, 213, 221, 1)',
+      boxShadow: '0px 0px 60px 0px rgba(35, 131, 96, 0.3)',
+    },
+    spotlight: '/assets/spotlight3.png',
+    tags: [
+      {
+        id: 1,
+        name: 'React.js',
+        path: '/assets/react.svg',
+      },
+      {
+        id: 2,
+        name: 'ExpressJS',
+        path: 'assets/expressjs.png',
+      },
+      {
+        id: 3,
+        name: 'NodeJS',
+        path: '/assets/nodejs.png',
+      },
+      {
+        id: 4,
+        name: 'MongoDB',
+        path: '/assets/db.png',
+      },
+    ],
+  },
+  {
+    title: 'Taskstr - A Task Listing App.',
+    desc: 'A task listing app where you can create tasks to stay updated for the upcoming schedule. It provides creating tasks, Updating and deleting at your own convenience.',
+    subdesc:
+      'Built with React.js, Node.js, MongoDB, Express.js, It ensures a smooth and secure listing experience with encrypted passwords, To meet the needs of modern users.',
+    href: 'https://drive.google.com/file/d/11SOQRoAXqW_WvuJOlMNa3nUsGPIzH_1O/view?usp=sharing',
+    texture: '/textures/project/project4.mp4',
+    logo: '/assets/task.png',
+    logoStyle: {
+      backgroundColor: '#0E1F38',
+      border: '0.2px solid #0E2D58',
+      boxShadow: '0px 0px 60px 0px #2F67B64D',
+    },
+    spotlight: '/assets/spotlight4.png',
+    tags: [
+      {
+        id: 1,
+        name: 'React.js',
+        path: '/assets/react.svg',
+      },
+      {
+        id: 2,
+        name: 'ExpressJS',
+        path: 'assets/expressjs.png',
+      },
+      {
+        id: 3,
+        name: 'NodeJS',
+        path: '/assets/nodejs.png',
+      },
+      {
+        id: 4,
+        name: 'MongoDB',
+        path: '/assets/db.png',
+      },
+    ],
+  },
+  {
+    title: 'Properties. - A RealEstate Property Dealing Website',
+    desc: 'Blockchain real-estate NFT Dapp, This innovative website streamlines property dealings between buyers and sellers, while also integrating key participants line lenders, inspectors and appraisers.',
+    subdesc:
+      'Built with Solidity, React, javascript and hardhat, it gives a smooth buying experience to the users with blockchain security.',
+    href: 'https://drive.google.com/file/d/1D_fiKeI6i_aIWko2_pvKMeOM_D7IdVKp/view?usp=sharing',
+    texture: '/textures/project/project5.mp4',
+    logo: '/assets/Ps.svg',
+    logoStyle: {
+      backgroundColor: '#1C1A43',
+      border: '0.2px solid #252262',
+      boxShadow: '0px 0px 60px 0px #635BFF4D',
+    },
+    spotlight: '/assets/spotlight5.png',
+    tags: [
+      
+      {
+        id: 1,
+        name: 'Solidity',
+        path: 'assets/solidity.png',
+      },
+      {
+        id: 2,
+        name: 'React.js',
+        path: '/assets/react.svg',
+      },
+      {
+        id: 3,
+        name: 'JavaScript',
+        path: '/assets/JS.png',
+      },
+      {
+        id: 4,
+        name: 'HardHat',
+        path: '/assets/hardhat.png',
+      },
+    ],
+  },
+];
+
+export const calculateSizes = (isSmall, isMobile, isTablet) => {
+  return {
+    deskScale: isSmall ? 0.05 : isMobile ? 0.06 : 0.065,
+    deskPosition: isMobile ? [0.5, -4.5, 0] : [0.25, -5.5, 0],
+    cubePosition: isSmall ? [4, -5, 0] : isMobile ? [5, -5, 0] : isTablet ? [5, -5, 0] : [9, -5.5, 0],
+    reactLogoPosition: isSmall ? [3, 4, 0] : isMobile ? [5, 4, 0] : isTablet ? [5, 4, 0] : [12, 3, 0],
+    ringPosition: isSmall ? [-5, 7, 0] : isMobile ? [-10, 10, 0] : isTablet ? [-12, 10, 0] : [-24, 10, 0],
+    targetPosition: isSmall ? [-5, -10, -10] : isMobile ? [-9, -10, -10] : isTablet ? [-11, -7, -10] : [-13, -13, -10],
+  };
+};
+
+export const workExperiences = [
+  {
+    id: 1,
+    name: 'NULL CLASS',
+    pos: 'Block-Chain Developer Intern',
+    duration: 'MAY 24 - JULY 4 | 2024',
+    title: "Creating Smart Contracts : Deadlines ( Create, Update ), Highest Funder ( Contributer ). Funding is successful or not. Number of successfully funded projects. Deduct A 5% Commission fee from each contribution.",
+    icon: '/assets/nullclass.webp',
+    animation: 'victory',
+  },
+];
